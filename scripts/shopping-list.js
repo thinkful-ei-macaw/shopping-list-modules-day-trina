519111a9804a3c556d0fad250f179e5d47693f2f
Trina <trinamceary@gmail.com>
2020-02-18 14:14:09 -0800

    throw/catch in addItemToShoppingList function

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -43,7 +43,15 @@ const render = function () {
 };
 
 const addItemToShoppingList = function (itemName) {
-  store.items.push({ id: cuid(), name: itemName, checked: false });
+  try {
+    item.validateName;
+    item.create;
+    store.items.push();
+    render();
+  }
+  catch {
+    console.log(`Cannot add item ${error.message}`);
+  }
 };
 
 const handleNewItemSubmit = function () {
