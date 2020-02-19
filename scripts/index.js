d3c31a36a2f43fe77f0a159bcf0425dfe1aa430f
Trina <trinamceary@gmail.com>
2020-02-19 08:40:06 -0800

    complete step 5, test code passes!

--- scripts/index.js
+++ scripts/index.js
@@ -8,3 +8,4 @@ const main = function () {
 };
 
 $(main);
+
--- scripts/store.js
+++ scripts/store.js
@@ -3,7 +3,49 @@ import item from './item.js';
  let items = [];
  let hideCheckedItems = false;
 
+ function findbyId(id){
+   items.find(id);
+   return id;
+ }
+
+ function addItem(name){
+   try {
+    item.validateName(name);
+    this.items.push(item.create(name))
+   }
+   catch (error){
+    console.log(`Cannot add item ${error.message}`)
+   }
+ };
+
+//is this right???
+ function findAndToggleChecked(id){
+  let toggle = this.findbyId(id);
+  toggle.checked = !toggle.checked;
+ }
+
+ function findAndUpdateName(id, newName){
+  try {
+    item.validateName(newName);
+    this.findbyId(id);
+  }
+  catch (error) {
+    console.log(`Cannot update name: ${error.message}`);
+  }
+ }
+
+ function findAndDelete(id){
+  this.items = this.items.filter(item => item.id !== id);
+ }
+
+
+
  export default {
     items,
-    hideCheckedItems
+    hideCheckedItems,
+    findbyId,
+    addItem,
+    findAndToggleChecked,
+    findAndUpdateName,
+    findAndDelete
   };
